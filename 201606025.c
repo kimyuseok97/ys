6b9be987d4c34ec825661514b676c7be323e9a9f
kimyuseok97 <62456807+kimyuseok97@users.noreply.github.com>
2020-03-22 19:49:17 +0900

    Add files via upload

---
+++ 201606025.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+main()
+{
+    printf ( "1)");
+    printf ( "\n" );
+    printf ( "¡á¡á¡á¡á");
+    printf ( "\n" );
+    printf ( "¡á¡á");
+    printf ( "\n" );
+    printf ( "¡á¡á");
+    printf ( "\n" );
+    printf ( "¡á¡á¡á¡á");
+    printf ( "\n" );
+    printf ( "\n" );
+    printf ( "2)");
+    printf ( "\n" );
+    printf ( "¡â" );
+    printf ( "\n" );
+    printf ( "¡â¡â" );
+    printf ( "\n" );
+    printf ( "¡â¡â¡â" );
+    printf ( "\n" );
+    printf ( "¡â¡â¡â¡â" );
+    printf ( "\n" );
+    printf ( "¡â¡â¡â¡â¡â" );
+}
